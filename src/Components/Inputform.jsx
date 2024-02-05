d3d4cabbe3a082c8812334f154b54f44709d32b9
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-02-04 16:33:13 -0800

    add css

--- src/Components/Inputform.jsx
+++ src/Components/Inputform.jsx
@@ -13,14 +13,20 @@ const Inputform = ({
 }) => {
   const amountInputId = useId();
   return (
-    <div>
-      <div>
-        <label htmlFor={amountInputId}>{label}</label>
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
         <input
           id={amountInputId}
           type="number"
           value={amount}
           placeholder="Amount"
+          className="outline-none w-full bg-transparent py-1.5"
           onChange={(e) => {
             if (e.target.value < 0) e.target.value = 0;
             onAmountChange && onAmountChange(e.target.value);
@@ -28,9 +34,10 @@ const Inputform = ({
           disabled={amountDisable}
         />
       </div>
-      <div>
-        <p>Currency Type</p>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
         <select
+          className="rounded-lg p-1 bg-gray-100 cursor-pointer outline-none"
           disabled={currencyDisable}
           value={selectCurrency}
           onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
